05339c7195e603268860d95bad7410b06989a235
mahoopresor <146128062+Machoopresor@users.noreply.github.com>
2024-06-22 03:41:10 -0600

    holis

---
+++ aws-exports.js
@@ -0,0 +1,11 @@
+const awsconfig = {
+    aws_project_region: 'us-east-2', // Región del proyecto
+    aws_cognito_identity_pool_id: 'us-east-2:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', // ID de la Identity Pool
+    aws_cognito_region: 'us-east-2', // Región del Cognito
+    aws_user_pools_id: 'us-east-2_9bAyL5ASI', // ID del grupo de usuarios
+    aws_user_pools_web_client_id: '2n951cu3eqgi7raov5srbp2ugn', // ID del cliente de la aplicación
+    oauth: {}, // Configuración OAuth, si es aplicable
+  };
+  
+  export default awsconfig;
+  
