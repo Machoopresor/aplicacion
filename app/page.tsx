cea5ec23dbcb4ac330f811df0631fce68ac5d213
mahoopresor <146128062+Machoopresor@users.noreply.github.com>
2024-06-23 01:56:28 -0600

    cuadro
    holis

--- app/page.tsx
+++ app/page.tsx
@@ -3,38 +3,46 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 import awsconfig from '../aws-exports'; // Asegúrate de que la ruta es correcta
-import { Authenticator, AuthenticatorProps } from '@aws-amplify/ui-react';
+import { Authenticator, ThemeProvider, defaultTheme } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import "./app.css";
 
 Amplify.configure(awsconfig);
 
-const formFields: AuthenticatorProps['formFields'] = {
-  signUp: {
-    email: {
-      placeholder: 'Enter your email',
-      isRequired: true,
-    }
+const theme = {
+  name: 'custom-theme',
+  tokens: {
+    components: {
+      authenticator: {
+        container: {
+          // Estos estilos se pueden quitar, ya que se manejarán en el CSS
+        },
+      },
+    },
   },
 };
 
 const MyComponent = () => {
   return (
-    <Authenticator formFields={formFields}>
-      {({ signOut, user }) => (
-        <main>
-          {user ? (
-            <>
-              <h1>Hello {user.username}</h1>
-              <button onClick={signOut}>Sign out</button>
-            </>
-          ) : (
-            <h1>Loading...</h1>
+    <ThemeProvider theme={{ ...defaultTheme, ...theme }}>
+      <div className="auth-container">
+        <Authenticator>
+          {({ signOut, user }) => (
+            <main>
+              {user ? (
+                <>
+                  <h1>Hello {user.username}</h1>
+                  <button onClick={signOut}>Sign out</button>
+                </>
+              ) : (
+                <h1>Loading...</h1>
+              )}
+            </main>
           )}
-        </main>
-      )}
-    </Authenticator>
+        </Authenticator>
+      </div>
+    </ThemeProvider>
   );
 };
 
